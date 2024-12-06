5625ef2441df78eb79760e8b4cbd7e57cedbeb6d
Fawas Ajani <G0041322@atu.ie>
2024-12-06 09:55:13 +0000

    Email

--- src/components/contact.js
+++ src/components/contact.js
@@ -93,7 +93,7 @@ const Contact = () => {
                             <FontAwesomeIcon icon={faPhone} className="fontawesome-icons" aria-hidden="true" />
                             <span className="contact-number">(032) 987 6543</span>
                             <FontAwesomeIcon icon={faEnvelope} className="fontawesome-icons" aria-hidden="true" />
-                            <span className="contact-email"><a href="mailto:Fawas Beans&Brews@example.com">Fawas Beans&Brews@yahoo.com</a></span>
+                            <span className="contact-email"><a href="mailto:Fawas Beans&Brews@yahoo.com">Fawas Beans&Brews@yahoo.com</a></span>   {/* Email-Fawas Beans&Brews@yahoo.com */}
                         </div>
                     </div>
                     <br />
